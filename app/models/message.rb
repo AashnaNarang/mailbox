fc1f3bd53e88306a170f60260ba62835b321ad20
Aashna <aashnanarang33@gmail.com>
2021-05-06 11:55:36 -0400

    created Message model

---
+++ app/models/message.rb
@@ -0,0 +1,10 @@
+Class Message < ActiveRecord::Base
+  belongs_to :conversations
+  belongs_to :user
+
+  validates_presence_of :body, :conversation_id, :user_id
+
+  def message_time
+    created_at.strtime("%m/%d/%y at %l:%M:%p")
+  end
+end
